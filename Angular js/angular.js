c9d6e51d7baa5241f46f099efd3ad7adff739de8
lakeesh10 <lakeesh10@gmail.com>
2016-07-12 01:02:03 +0530

    Add files via upload

--- Angular js/angular.js
+++ Angular js/angular.js
@@ -13,16 +13,26 @@ var app = angular.module('my-app', [], function () {})
 window.names1=[];
 window.country={};
 app.controller('MainController', function($scope, $http) {
+  $scope.keyValue="India";
     $http.get("http://country.io/names.json").success(function(response) {
         $scope.names = response; 
         window.names1.push(response);
         window.country=response;
-        var key = country.getKey('India');
-        console.log(key);
+        
+  
+        
     });
+     
     $http.get("http://country.io/iso3.json").success(function(response) {$scope.iso= response; window.names1.push(response)});
     $http.get("http://country.io/capital.json").success(function(response) {$scope.capital = response; window.names1.push(response)});
     $http.get("http://country.io/phone.json").success(function(response) {$scope.phone = response; window.names1.push(response)});
     $http.get("http://country.io/currency.json").success(function(response) {$scope.currency = response; window.names1.push(response)});
+    
+     $scope.$watch("keyValue", function(newValue, oldValue) {
+       $scope.key = country.getKey(newValue);
+     console.log($scope.key);
+   });
+    
+    
 });
 
