28c5842aac6f323362c90d0dfb873489ec4df4e7
lakeesh10 <lakeesh10@gmail.com>
2016-07-11 23:18:11 +0530

    Add files via upload

--- Angular js/angular.js
+++ Angular js/angular.js
@@ -1,24 +1,40 @@
 //http://stackoverflow.com/questions/20035101/no-access-control-allow-origin-header-is-present-on-the-requested-resource
 //http://country.io/data/
 
-var app = angular.module('my-app', [], function () {})
-
-app.controller('MainController', ['users', function (users) {
-    var vm = this;
-    users.get(function (data) {
-        vm.users = data;
-        window.names1.push(data);
-    });
-    vm.title = 'Hello World';
-}]);
-window.names1=[];
-
-app.factory('users', ['$http', function ($http) {
-    return {
-        get: function (callback) {
-            $http.get('http://country.io/names.json').success(function (data) {
-                callback( data);
-            })
-        }
+Object.prototype.getKey = function(value){
+  for(var key in this){
+    if(this[key] == value){
+      return key;
     }
-}]);
+  }
+  return null;
+};
+var app = angular.module('my-app', [], function () {})
+window.names1=[];
+window.country={};
+app.controller('MainController', function($scope, $http) {
+    $http.get("http://country.io/names.json").success(function(response) {
+        $scope.names = response; 
+        window.names1.push(response);
+        window.country=response;
+        var key = country.getKey('India');
+        console.log(key);
+    });
+    $http.get("http://country.io/iso3.json").success(function(response) {$scope.iso= response; window.names1.push(response)});
+    $http.get("http://country.io/capital.json").success(function(response) {$scope.capital = response; window.names1.push(response)});
+    $http.get("http://country.io/phone.json").success(function(response) {$scope.phone = response; window.names1.push(response)});
+    $http.get("http://country.io/currency.json").success(function(response) {$scope.currency = response; window.names1.push(response)});
+});
+
+
+var languageCodes = {
+  DA: 'Danish',
+  DE: 'German',
+  DZ: 'Bhutani',
+  EL: 'Greek',
+  EN: 'English',
+  EO: 'Esperanto',
+  ES: 'Spanish'
+};
+window.c=languageCodes;
+
